cc938bf96dd26c3592e9560d8d54702ee803cb7a
Swarnim <swarnim14.9@hotmail.com>
2025-12-27 14:38:04 +0400

    docs: better intro commentary for melpa

--- turepo.el
+++ turepo.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Swarnim Barapatre <swarnim14.9@hotmail.com>
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "28.1"))
-;; Keywords: vc, git, convenience
+;; Keywords: vc, git, convenience, github
 ;; URL: https://github.com/swarnimcodes/turepo
 
 ;; This file is not part of GNU Emacs.
@@ -28,14 +28,23 @@
 
 ;; turepo provides a simple command to open the current project's
 ;; git repository in your web browser.  It supports:
-;; - GitHub (SSH and HTTPS)
-;; - GitLab (SSH and HTTPS)
-;; - SSH aliases (github-work, gitlab-personal, etc.)
+;; - GitHub
+;; - GitLab
+;; - Codeberg
+;; - SourceHut
+;; - Gitea
+;; Both ssh based and http based git projects are supported
 
 ;; Usage:
-;;   M-x turepo
-;; Or bind it to a key:
-;;   (global-set-key (kbd "C-c g r") 'turepo)
+
+;; Via use-package:
+;; (use-package turepo
+;;   :ensure t
+;;   :bind (("C-c g r" . turepo)))
+
+;; Or directly via the `turepo` function provided by the package.
+
+;; Developed by Swarnim B (https://smarniw.com)
 
 ;;; Code:
 (require 'vc)
